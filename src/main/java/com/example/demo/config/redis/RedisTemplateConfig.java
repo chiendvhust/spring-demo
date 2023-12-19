8984da62cf284f6bd7abe0a023363f8d55b35f34
Sotatek-ChienDao23 <chien.dao@sotatek.com>
2023-12-19 16:19:08 +0700

    feat: add operation

--- src/main/java/com/example/demo/config/redis/RedisTemplateConfig.java
+++ src/main/java/com/example/demo/config/redis/RedisTemplateConfig.java
@@ -3,7 +3,7 @@ package com.example.demo.config.redis;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.*;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 
 @Configuration
@@ -17,5 +17,31 @@ public class RedisTemplateConfig {
 		redisTemplate.setDefaultSerializer(new GenericJackson2JsonRedisSerializer());
 		redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
 		return redisTemplate;
+	}
+
+	@Bean
+	<V> ValueOperations<String, V> valueOperations(final RedisTemplate<String, V> redisTemplate) {
+		return redisTemplate.opsForValue();
+	}
+
+	@Bean
+	<V> SetOperations<String, V> setOperations(final RedisTemplate<String, V> redisTemplate) {
+		return redisTemplate.opsForSet();
+	}
+
+	@Bean
+	<V> ZSetOperations<String, V> zSetOperations(final RedisTemplate<String, V> redisTemplate) {
+		return redisTemplate.opsForZSet();
+	}
+
+	@Bean
+	<V> ListOperations<String, V> listOperations(final RedisTemplate<String, V> redisTemplate) {
+		return redisTemplate.opsForList();
+	}
+
+	@Bean
+	<HK, V> HashOperations<String, HK, V> hashOperations(
+			final RedisTemplate<String, V> redisTemplate) { //NOSONAR
+		return redisTemplate.opsForHash();
 	}
 }
